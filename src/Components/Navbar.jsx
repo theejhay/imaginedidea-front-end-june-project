d36159d0c84601e9ac8167c0bdc6cce77fcda63c
alaobasit <alaooluwapelumi222@gmail.com>
2025-10-07 07:32:47 +0100

    commit all changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from'../assets/images/logo.png';
+import React, { useState } from 'react';
+import logo from '../assets/images/Logo.png';
 import './Navbar.css';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <img src={logo} alt="Brooklyn Logo" className="logo-img" />
       </div>
-      <ul className="navbar-links">
+
+      <button className="hamburger" onClick={toggleMenu}>
+        <span className={isOpen ? 'bar open' : 'bar'}></span>
+        <span className={isOpen ? 'bar open' : 'bar'}></span>
+        <span className={isOpen ? 'bar open' : 'bar'}></span>
+      </button>
+
+      <ul className={`navbar-links ${isOpen ? 'active' : ''}`}>
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Process</a></li>
         <li><a href="#">Portfolio</a></li>
         <li><a href="#">Blog</a></li>
         <li><a href="#">Services</a></li>
-        <li>
-          <button className="contact-btn">Contact</button>
-        </li>
+        <li><button className="contact-btn">Contact</button></li>
       </ul>
     </nav>
   );
