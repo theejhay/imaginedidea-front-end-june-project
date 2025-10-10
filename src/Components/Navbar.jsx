1ec4417e2485c170fdd6b033521b2bc994cce0f8
alaobasit <alaooluwapelumi222@gmail.com>
2025-10-10 21:20:06 +0100

    commit new update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,40 +10,29 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-header">
+        {/* Logo */}
         <div className="navbar-logo">
-          <img src={logo} alt="Logo" />
+          <img src={logo} alt="Logo" className="logo-img" />
         </div>
 
-        <button className="hamburger" onClick={toggleMenu}>
+        {/* Hamburger menu (visible on mobile) */}
+        <button className="hamburger" onClick={toggleMenu} aria-label="Toggle menu">
           <span className={isOpen ? "bar open" : "bar"}></span>
           <span className={isOpen ? "bar open" : "bar"}></span>
           <span className={isOpen ? "bar open" : "bar"}></span>
         </button>
-      </div>
 
-      <ul className={`navbar-links ${isOpen ? "active" : ""}`}>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Process</a>
-        </li>
-        <li>
-          <a href="#">Portfolio</a>
-        </li>
-        <li>
-          <a href="#">Blog</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li>
-          <button className="contact-btn">Contact</button>
-        </li>
-      </ul>
+        {/* Navigation links */}
+        <ul className={`navbar-links ${isOpen ? "active" : ""}`}>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Process</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Services</a></li>
+          <li><button className="contact-btn">Contact</button></li>
+        </ul>
+      </div>
     </nav>
   );
 }
