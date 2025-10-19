eaef0a909fe0a6a0ec51580f650c664439548c18
alaobasit <alaooluwapelumi222@gmail.com>
2025-10-19 11:43:46 +0100

    commit and push

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Hero from './Components/Usercard.jsx';
+import Usercard from './Components/Usercard.jsx'; 
 import './App.css';
 
 function App() {
   return (
-    <Usercard/>
+    <Usercard /> 
   );
 }
 
