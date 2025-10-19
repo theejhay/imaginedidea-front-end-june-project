dc2e6d461241679c8b7453541631d95f429207c6
abdul damilola <abduldamilola79@gmail.com>
2025-10-19 16:20:50 +0100

    reinitialized commits

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Hero from './components/Hero';
+import './App.css';
+import ContactForm from './components/ContactForm';
+
+function App() {
+  return (
+    <>
+  
+      <Hero/>
+      <ContactForm />
+    </>
+   
+  );
+}
+
+export default App;
