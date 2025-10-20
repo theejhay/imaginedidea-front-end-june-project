d978d3e6f1f37ebc3f7075f4158564f2d7815c6f
alaobasit <alaooluwapelumi222@gmail.com>
2025-10-20 13:51:07 +0100

    commit and push all new changes

--- src/Components/Usercard.jsx
+++ src/Components/Usercard.jsx
@@ -1,40 +1,48 @@
-import React from 'react';
-import './Usercard.css';
+import React from "react";
+import ProfileImage from "../assets/images/ProfileImage.jpg";
+import SocialMedia1 from "../assets/images/SocialMedia1.png";
+import SocialMedia2 from "../assets/images/SocialMedia2.png";
+import SocialMedia3 from "../assets/images/SocialMedia3.png";
+import SocialMedia4 from "../assets/images/SocialMedia4.png";
+import SocialMedia5 from "../assets/images/SocialMedia5.png";
+import "./Usercard.css";
 
 const Usercard = () => {
   return (
     <div className="user-card-container">
-      <div className="row align-items-center">
-        {/* Profile Section */}
-        <div className="col-md-5 text-center">
-          <img
-            src="/assets/images/profile.jpg"
-            alt="User"
-            className="profile-img"
-          />
-          <div className="icon-row mt-4">
-            <img src="/assets/images/Social_media_card_1.png" alt="LinkedIn" />
-            <img src="/assets/images/Social_media_card_2.png" alt="Instagram" />
-            <img src="/assets/images/Social_media_card_3.png" alt="Facebook" />
-            <img src="/assets/images/Social_media_card_4.png" alt="Behance" />
-          </div>
-        </div>
+      {/* Profile Section */}
+      <div className="profile-section">
+        <img src={ProfileImage} alt="User" className="profile-img" />
 
-        {/* Text Section */}
-        <div className="col-md-7">
-          <h2 className="user-title">I design and develop services for customers</h2>
-          <p className="user-description">
-            Specializing in creating stylish, modern websites, web services, and online stores.
-            My passion is to design digital user experiences.
-          </p>
-          <p className="user-description">
-            Specializing in creating stylish, modern websites, web services, and online stores.
-            My passion is to design digital user experiences.
-          </p>
-          <div className="button-group mt-4">
-            <a href="#projects" className="btn btn-purple me-3">My Project</a>
-            <a href="/cv.pdf" className="btn btn-pink">Download CV</a>
-          </div>
+        <div className="social-media-card">
+          <img src={SocialMedia4} alt="Instagram" />
+          <img src={SocialMedia3} alt="LinkedIn" />
+          <img src={SocialMedia2} alt="Facebook" />
+          <img src={SocialMedia1} alt="Behance" />
+          <img src={SocialMedia5} alt="Media" />
+        </div>
+      </div>
+
+      {/* Text Section */}
+      <div className="text-section">
+        <p className="user-description">
+          I design and develop services for customers specializing in creating
+          stylish, modern websites, web services, and online stores. My passion
+          is to design digital user experiences.
+        </p>
+
+        <p className="user-short">
+          I design and develop services for customers specializing in creating
+          stylish, modern websites, web services.
+        </p>
+        <div className="button-group">
+          <a href="#projects" className="btn btn-project">
+            My Project
+          </a>
+          <a href="/cv.pdf" className="btn btn-download">
+            <i className="bi bi-download me-2"></i>
+            Download CV
+          </a>
         </div>
       </div>
     </div>
