b29d35631a2832c6d02e6c7c01bd39aef8f7e397
abdul damilola <abduldamilola79@gmail.com>
2025-10-21 04:28:55 +0100

    Added contact form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Hero from './components/Hero';
 import './App.css';
+import ContactForm from './Components/ContactForm';
 
 function App() {
   return (
-    <Hero/>
+    <><Hero /><ContactForm /></>
   );
 }
 
