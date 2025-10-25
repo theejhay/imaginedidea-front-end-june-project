f4cd7ebb856cf51667a67cb79a31a7a2de103d14
Taiwo Ogunyemi <ogunyemitaiwojohn@gmail.com>
2025-10-25 14:09:44 +0100

    Responsiveness fixed | code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,10 @@
-import {useState} from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import pen from "./assets/pen.png";
-import graph from "./assets/graph.png";
-import laptop from "./assets/laptop.png";
-import note from "./assets/note.png";
+import WorkProcess from "./components/WorkProcess";
 
 function App() {
   return (
     <>
-      <div className="work-process-border">
-        <div className="work-process-component">
-          <div className="work-process-component-1">
-            <div className="work-process-component-1-title">Work Process</div>
-            <div className="work-process-component-1-description">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                purus arcu, varius eget velit non, laoreet imperdiet orci.
-                Mauris ultrices eget lorem ac vestibulum. Suspendis imperdiet,
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                purus arcu, varius eget velit non.
-              </p>
-            </div>
-          </div>
-          <div className="work-process-component-2">
-            <div className="work-process-component-2-1">
-              <img src={note} alt="" />
-              <div className="details">
-                <h1>1. Research</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                  purus arcu.
-                </p>
-              </div>
-            </div>
-            <div className="work-process-component-2-2">
-              <img src={graph} alt="" />
-              <div className="details">
-                <h1>2. Analysis</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                  purus arcu.
-                </p>
-              </div>
-            </div>
-            <div className="work-process-component-2-3">
-              <img src={pen} alt="" />
-              <div className="details">
-                <h1>3. Design</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                  purus arcu.
-                </p>
-              </div>
-            </div>
-            <div className="work-process-component-2-4">
-              <img src={laptop} alt="" />
-              <div className="details">
-                <h1>4. Launch</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                  purus arcu.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <WorkProcess />
     </>
   );
 }
