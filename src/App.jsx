1216c3d9d1f68164f28caaedc0563e76105b5f08
Simi <similoluwabonafide@gmail.com>
2025-10-31 12:39:56 +0000

    commit portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import WorkProcess from "./components/WorkProcess";
 import ContactForm from './components/ContactForm';
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
     <>
-      <WorkProcess />
-      <ContactForm />
+      {/* <WorkProcess />
+      <ContactForm /> */}
+      <Portfolio />
+
     </>
     
   );
