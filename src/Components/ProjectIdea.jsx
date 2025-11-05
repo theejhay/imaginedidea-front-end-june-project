0c4ec08e648b1428861d56cf5a9b6774fc2e9fa6
Simi <similoluwabonafide@gmail.com>
2025-11-05 20:49:49 +0000

    project idea commit

--- src/Components/ProjectIdea.jsx
+++ src/Components/ProjectIdea.jsx
@@ -3,12 +3,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 function ProjectIdea() {
   return (
-    <div className="project-idea-container col">
-      <h2 className="heading col-sm-auto">
+    <div className="project-idea-container col-12 col-sm-6 col-md-4 mb-4">
+      <h2 className="heading">
         Do you have Project Idia? Let's discuss your project!
       </h2>
 
-      <p className="sub-heading col-sm-auto">
+      <p className="sub-heading">
         There are many variations of passages of Lorem Ipsum available, but the
         majority have suffered alteration.
       </p>
