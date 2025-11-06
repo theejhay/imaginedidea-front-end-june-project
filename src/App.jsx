53c4191d81a5cb2b8beeab6f3a237e39d52b19f2
Tomiwa Famuti <famutitomiwa@gmail.com>
2025-11-06 01:18:41 +0000

    Remove errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
+import Blog from "./components/Blog";
+import ContactForm from "./components/ContactForm";
 import WorkProcess from "./components/WorkProcess";
-import ContactForm from './components/ContactForm';
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
       <WorkProcess />
       <ContactForm />
+      <Blog />
     </>
-    
   );
 }
 
