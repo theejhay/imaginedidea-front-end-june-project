e4dff26c9a3f4831c66f0dc1d396f009e80bbffe
Simi <similoluwabonafide@gmail.com>
2025-11-07 20:56:15 +0000

    updated projectIdea import

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Blog from "./components/Blog";
 import ContactForm from "./components/ContactForm";
-import ProjectIdea from "./components/ProjectIdea";
 import PortfolioSection from "./components/PortfolioSection";
 import WhatIDo from "./components/WhatIDo";
 import HappyClient from "./components/HappyClient";
 import Testimonial from "./components/Testimonial";
+import ProjectIdeas from "./components/ProjectIdeas";
 
 function App() {
   return (
     <>
       <ContactForm />
-      <ProjectIdea/>
+      <ProjectIdeas/>
       <HappyClient />
       <Testimonial />
       <PortfolioSection />
