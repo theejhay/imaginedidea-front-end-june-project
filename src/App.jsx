4439566b1f8880da70967392806043eb2fdc1b0e
Simi <similoluwabonafide@gmail.com>
2025-11-07 18:45:30 +0000

    happy client  section commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import "./App.css";
 import Blog from "./components/Blog";
 import ContactForm from "./components/ContactForm";
 import WorkProcess from "./components/WorkProcess";
-import HappyClient from "./components/HappyClient";
-import Testimonial from "./components/Testimonial";
 import WhatIDo from "./components/WhatIDo";
-
 import "bootstrap/dist/css/bootstrap.min.css";
+import HappyClient from "./Components/HappyClient";
+import Testimonial from "./Components/Testimonial";
 
 function App() {
   return (
