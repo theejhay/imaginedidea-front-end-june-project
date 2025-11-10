ac2dc1e10994e6154fc8cf403dabb7963335b4b7
Tomiwa Famuti <famutitomiwa@gmail.com>
2025-11-10 20:31:27 +0000

    updated

--- src/components/FootSec.jsx
+++ src/components/FootSec.jsx
@@ -7,32 +7,22 @@ function FootSec() {
   return (
     <>
       <footer>
-        <div className="container-fluid main-container ">
-          <div className="row align-items-center text-center text-md-start ">
-            <div className="col-md-3 mb-3 mb-md-0 d-flex ">
-              <div className="footer-logo justify-content-md-start justify-content-center d-flex align-items-center">
-                <div className="logo-circle">
-                  <img src={logo} alt="" />
-                </div>
-                <span className="">Brooklyn</span>
-              </div>
-            </div>
-
-            <div className="col-md-6 mb-3 mb-md-0 ">
-              <nav className="footer-nav d-flex justify-content-center flex-wrap">
-                <a href="#">Home</a>
-                <a href="#">About</a>
-                <a href="#">Services</a>
-                <a href="#">Process</a>
-                <a href="#">Portfolio</a>
-                <a href="#">Blog</a>
-                <a href="#">Contact</a>
-              </nav>
-            </div>
-
-            <div className="col-md-3 text-md-end">
-              <small>Copyright © 2022 Picto.</small>
-            </div>
+        <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
+          <div class="d-flex align-items-center mb-3 mb-md-0">
+            <div class="logo-circle">B</div>
+            <span class="fw-bold text-white fs-5">Brooklyn</span>
+          </div>
+          <nav class="footer-nav d-flex flex-wrap justify-content-center mb-3 mb-md-0">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Process</a>
+            <a href="#">Portfolio</a>
+            <a href="#">Blog</a>
+            <a href="#">Contact</a>
+          </nav>
+          <div class="text-center text-md-end">
+            <small>Copyright © 2022 Picto.</small>
           </div>
         </div>
       </footer>
